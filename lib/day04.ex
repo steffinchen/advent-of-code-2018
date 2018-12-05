bc26a55e92ad80118d14c151cf1b55ebcb5da6f5
Stefanie Wehrle <steffinchen@gmail.com>
2018-12-05 17:38:08 +0000

    :sparkles: Day four, part two

--- lib/day04.ex
+++ lib/day04.ex
@@ -1,22 +1,46 @@
 defmodule Day04 do
 
     def a do
-        log_entries =
-            get_input()
-            |> Enum.reduce(%{guard: nil, fell_asleep_at: nil, consolidated: []}, &consolidate_sleep/2)
+        log_entries = get_input()
 
-        Map.get(log_entries, :consolidated)
-        |> find_guard_with_highest_total
+        find_guard_with_highest_total(log_entries)
         |> elem(0)
-        |> find_min_most_asleep_at(Map.get(log_entries, :consolidated))
+        |> find_min_most_asleep_at(log_entries)
         |> calc_result
     end
 
     def b do
+        get_input()
+        |> count_minutes_by_guard
+        |> find_guard_with_highes_minute
+        |> calc_result
+    end
 
+    defp find_guard_with_highes_minute(minutes_by_guard) do
+        Enum.reduce(minutes_by_guard, %{},
+            fn {guard, min_guard}, acc ->
+                {min, count} = Enum.max_by(min_guard, fn {_, count} -> count end)
+                Map.put(acc, guard, %{minute: min, count: count})
+            end)
+       |> Enum.max_by(fn {_, %{minute: _, count: count}} -> count end)
+    end
+
+    defp count_minutes_by_guard(entries) do
+        Enum.reduce(entries, %{},
+        fn entry, minutes ->
+            Enum.reduce(entry.fell_asleep_at .. entry.woke_up-1,
+                minutes,
+                fn minute, acc ->
+                    Map.update(acc, entry.guard, %{}, fn val -> Map.update(val, minute, 1, &(&1 + 1)) end)
+                end)
+        end)
     end
 
     defp calc_result(%{guard: guard, minute: minute}) do
+        guard * minute
+    end
+
+    defp calc_result({guard, %{count: _, minute: minute}}) do
         guard * minute
     end
 
@@ -80,6 +104,8 @@ defmodule Day04 do
         |> String.split("\r\n")
         |> Enum.map_reduce(0, &parse_input/2)
         |> elem(0)
+        |> Enum.reduce(%{guard: nil, fell_asleep_at: nil, consolidated: []}, &consolidate_sleep/2)
+        |> Map.get(:consolidated)
     end
 
 end
