a54604aa0b7918cca9507b8f3bb32855e5aad6c8
Stefanie Wehrle <steffinchen@gmail.com>
2018-12-06 17:30:02 +0000

    :zap: use list comprehension for Day 5

--- lib/day05.ex
+++ lib/day05.ex
@@ -2,28 +2,32 @@ defmodule Day05 do
 
     def a do
         get_input()
+        |> String.graphemes
         |> remove_opposite_polarity
-        |> String.length
+        |> Enum.count
     end
 
     def b do
         word = get_input()
         Enum.map(?a..?z, fn(x) -> <<x :: utf8>> end)
-        |> Enum.reduce(%{}, 
+        |> Enum.reduce(%{},
             fn letter, acc ->
-                IO.inspect(letter)
-                count = String.replace(word, letter, "")
-                    |> String.replace(String.upcase(letter), "")
+                count = remove_all(word, letter)
+                    |> String.graphemes
                     |> remove_opposite_polarity
-                    |> String.length
+                    |> Enum.count
                 Map.put(acc, letter, count)
             end)
         |> IO.inspect
         |> Enum.min_by(fn {_, count} -> count end)
     end
 
+    defp remove_all(word, letter) do
+        String.replace(word, ~r/[#{String.downcase(letter)}#{String.upcase(letter)}]/, "")
+    end
+
     defp remove_opposite_polarity(word, previous_count) do
-        if previous_count == String.length(word) do
+        if previous_count == Enum.count(word) do
             word
         else
             remove_opposite_polarity(word)
@@ -31,34 +35,26 @@ defmodule Day05 do
     end
 
     defp remove_opposite_polarity(word) do
-        graphemes = String.graphemes(word)
-        Stream.with_index(graphemes)
-        |> Enum.reduce(graphemes, &check_letters/2)
-        |> Enum.join("")
-        |> String.replace("_", "")
-        |> remove_opposite_polarity(String.length(word))
+        word
+        |> Enum.reduce([], &react/2)
+        |> remove_opposite_polarity(Enum.count(word))
     end
 
-    defp check_letters({_, 0}, acc) do
-        acc
-    end
-
-    defp check_letters({curr, n}, acc) do
-        prev = Enum.at(acc, n-1)
+    defp react(curr, [prev | tail] = acc) do
         if curr != prev && (String.upcase(curr) == prev || String.downcase(curr) == prev) do
-            List.replace_at(acc, n, "_")
-            |> List.replace_at(n-1, "_")
+            tail
         else
-            acc
+            [curr | acc]
         end
+    end
+
+    defp react(curr, []) do
+        [curr]
     end
 
     defp get_input do
         File.read!("input/input_05.txt")
-        # "aacabdaAeAafAA"
-        #"dabAcCaCBAcCcaDA"
-        # "abAB"
-        #"aabAAB"
+        # "dabAcCaCBAcCcaDA"
     end
 
 end
