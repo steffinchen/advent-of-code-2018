94267bce51c1a22a5d45b581fc94d48269f5a7f8
Stefanie Wehrle <steffinchen@gmail.com>
2018-12-18 09:24:25 +0000

    :sparkles: finished Day 10

--- lib/day10.ex
+++ lib/day10.ex
@@ -1,22 +1,34 @@
 defmodule Day10 do
 
     def a do
-        get_input()
-        |> step(2)
+        steps =
+            get_input()
+            |> find_smallest_bounding_box(nil, 0, 999999999999999)
+        {"Part 2", steps-1}
     end
 
-    def b do
+    defp find_smallest_bounding_box(input, previous_input, step_count, prev_size) do
+        {minX, maxX} = find_min_max(input, :posX)
+        {minY, maxY} = find_min_max(input, :posY)
+
+        lenX = maxX - minX + 1
+        lenY = maxY - minY + 1
+
+        bounding_box = lenX * lenY
+
+        if (bounding_box < prev_size) do
+            calc_next(input)
+            |> find_smallest_bounding_box(input, step_count + 1, bounding_box)
+        else
+            print(previous_input)
+            step_count
+        end
     end
 
-    defp step(input, 0) do
-        "done"
-    end
 
-    defp step(input, count) do
-        print(input)
-
-        calc_next(input)
-        |> step(count-1)
+    defp find_min_max(input, key) do
+        {min, max} = Enum.min_max_by(input, fn entry -> entry[key] end)
+        {min[key], max[key]}
     end
 
     defp calc_next(input) do
@@ -29,45 +41,53 @@ defmodule Day10 do
     end
 
     defp print(points) do
-        Enum.reduce(points, Matrix.new(16, 16, " "), fn %{posX: x, posY: y}, acc ->
-            Matrix.set(acc, y, x, "#")
+        IO.inspect("Part 1:")
+
+        {minX, maxX} = find_min_max(points, :posX)
+        {minY, maxY} = find_min_max(points, :posY)
+
+        lenX = maxX - minX + 1
+        lenY = maxY - minY + 1
+
+        Enum.reduce(points, Matrix.new(lenY, lenX, " "), fn %{posX: x, posY: y}, acc ->
+            Matrix.set(acc, y-minY, x-minX, "#")
         end)
         |> Enum.each(fn line -> IO.inspect(Enum.join(line, "")) end)
     end
 
     defp get_input do
-       # File.read!("input/input_10.txt")
-"position=< 9,  1> velocity=< 0,  2>
-position=< 7,  0> velocity=<-1,  0>
-position=< 3, -2> velocity=<-1,  1>
-position=< 6, 10> velocity=<-2, -1>
-position=< 2, -4> velocity=< 2,  2>
-position=<-6, 10> velocity=< 2, -2>
-position=< 1,  8> velocity=< 1, -1>
-position=< 1,  7> velocity=< 1,  0>
-position=<-3, 11> velocity=< 1, -2>
-position=< 7,  6> velocity=<-1, -1>
-position=<-2,  3> velocity=< 1,  0>
-position=<-4,  3> velocity=< 2,  0>
-position=<10, -3> velocity=<-1,  1>
-position=< 5, 11> velocity=< 1, -2>
-position=< 4,  7> velocity=< 0, -1>
-position=< 8, -2> velocity=< 0,  1>
-position=<15,  0> velocity=<-2,  0>
-position=< 1,  6> velocity=< 1,  0>
-position=< 8,  9> velocity=< 0, -1>
-position=< 3,  3> velocity=<-1,  1>
-position=< 0,  5> velocity=< 0, -1>
-position=<-2,  2> velocity=< 2,  0>
-position=< 5, -2> velocity=< 1,  2>
-position=< 1,  4> velocity=< 2,  1>
-position=<-2,  7> velocity=< 2, -2>
-position=< 3,  6> velocity=<-1, -1>
-position=< 5,  0> velocity=< 1,  0>
-position=<-6,  0> velocity=< 2,  0>
-position=< 5,  9> velocity=< 1, -2>
-position=<14,  7> velocity=<-2,  0>
-position=<-3,  6> velocity=< 2, -1>"
+        File.read!("input/input_10.txt")
+# "position=< 9,  1> velocity=< 0,  2>
+# position=< 7,  0> velocity=<-1,  0>
+# position=< 3, -2> velocity=<-1,  1>
+# position=< 6, 10> velocity=<-2, -1>
+# position=< 2, -4> velocity=< 2,  2>
+# position=<-6, 10> velocity=< 2, -2>
+# position=< 1,  8> velocity=< 1, -1>
+# position=< 1,  7> velocity=< 1,  0>
+# position=<-3, 11> velocity=< 1, -2>
+# position=< 7,  6> velocity=<-1, -1>
+# position=<-2,  3> velocity=< 1,  0>
+# position=<-4,  3> velocity=< 2,  0>
+# position=<10, -3> velocity=<-1,  1>
+# position=< 5, 11> velocity=< 1, -2>
+# position=< 4,  7> velocity=< 0, -1>
+# position=< 8, -2> velocity=< 0,  1>
+# position=<15,  0> velocity=<-2,  0>
+# position=< 1,  6> velocity=< 1,  0>
+# position=< 8,  9> velocity=< 0, -1>
+# position=< 3,  3> velocity=<-1,  1>
+# position=< 0,  5> velocity=< 0, -1>
+# position=<-2,  2> velocity=< 2,  0>
+# position=< 5, -2> velocity=< 1,  2>
+# position=< 1,  4> velocity=< 2,  1>
+# position=<-2,  7> velocity=< 2, -2>
+# position=< 3,  6> velocity=<-1, -1>
+# position=< 5,  0> velocity=< 1,  0>
+# position=<-6,  0> velocity=< 2,  0>
+# position=< 5,  9> velocity=< 1, -2>
+# position=<14,  7> velocity=<-2,  0>
+# position=<-3,  6> velocity=< 2, -1>"
         |> String.replace(" ", "")
         |> String.split("\n")
         |> Enum.map(fn line ->
